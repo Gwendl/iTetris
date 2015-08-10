d3b087aa6eba94871f1f16d0652061f5f9bc066e
gwendal lasson <gwendal@gwendals-macbook-air.local>
2015-08-10 21:01:48 +0200

    Initial Commit

---
+++ iTetrisTests/iTetrisTests.swift
@@ -0,0 +1,36 @@
+//
+//  iTetrisTests.swift
+//  iTetrisTests
+//
+//  Created by gwendal lasson on 10/08/15.
+//  Copyright © 2015 gwendal lasson. All rights reserved.
+//
+
+import XCTest
+@testable import iTetris
+
+class iTetrisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
